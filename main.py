f4cb371e1ca0c32bcfffd5f230521ae3d4729e91
Corentin Girard <corentingirard.dev@gmail.com>
2019-10-01 11:34:41 +0200

    Now use a per class file and import in main

---
+++ main.py
@@ -0,0 +1,27 @@
+from turing import Turing
+
+somme_unaires = Turing(
+    2, 
+    ['B', 'B', 1, 1, 1, 1, 1, 'B', 1, 1, 1, 1, 1, 1, 1, 1, 'B', 'B'], 
+    {
+        "initial": 0,
+        0: {
+            1: (0, None, 'R'),
+            'B': (1, 1, 'N')
+        },
+        1: {
+            1: (1, None, 'L'),
+            'B': (2, None, 'R'),
+        },
+        2: {
+            1: (3, 'B', 'R')
+        },
+        3: {
+            1: (4, 'B', 'R')
+        },
+        4: None # état final
+    }
+)
+
+while somme_unaires.step():
+    print(somme_unaires.bande)
--- turing.py
+++ turing.py
@@ -24,29 +24,3 @@ class Turing():
         self.etat = transition[0]
 
         return True # encore des étapes
-
-somme_unaires = Turing(
-    2, 
-    ['B', 'B', 1, 1, 1, 1, 1, 'B', 1, 1, 1, 1, 1, 1, 1, 1, 'B', 'B'], 
-    {
-        "initial": 0,
-        0: {
-            1: (0, None, 'R'),
-            'B': (1, 1, 'N')
-        },
-        1: {
-            1: (1, None, 'L'),
-            'B': (2, None, 'R'),
-        },
-        2: {
-            1: (3, 'B', 'R')
-        },
-        3: {
-            1: (4, 'B', 'R')
-        },
-        4: None # état final
-    }
-)
-
-while somme_unaires.step():
-    print(somme_unaires.bande)
