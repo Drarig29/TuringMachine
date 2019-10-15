cdc008a2aeea53decf39928bac235863c6760b74
SuperFola <lexplt.dev@gmail.com>
2019-10-15 11:59:49 +0200

    adding alphabet

--- main.py
+++ main.py
@@ -23,41 +23,50 @@ from turing import Turing
 #     4: None # état final
 # }
 
-turing = Turing(
-    10,
-    list("BBB1011#110BBBB"), # conversion de chaîne à tableau de caractères (nouvelle façon de faire)
-    {
-        "initial": 0,
-        0: {
-            '0': (0, '1', 'L'),
-            '1': (1, '0', 'L'),
-            'B': (0, None, 'L'),
-            '#': (4, None, 'R')
+if __name__ == '__main__':
+    turing = Turing(
+        # position de la tete de lecture
+        10,
+        # conversion de chaîne à tableau de caractères (nouvelle façon de faire)
+        list("BBB1011#110BBBB"),
+        # graphe
+        {
+            "initial": 0,
+            0: {
+                '0': (0, '1', 'L'),
+                '1': (1, '0', 'L'),
+                'B': (0, None, 'L'),
+                '#': (4, None, 'R')
+            },
+            1: {
+                '0': (1, None, 'L'),
+                '1': (1, None, 'L'),
+                '#': (2, None, 'L')
+            },
+            2: {
+                '0': (3, '1', 'N'),
+                '1': (2, '0', 'L'),
+                'B': (3, '1', 'N')
+            },
+            3: {
+                '0': (3, None, 'R'),
+                '1': (3, None, 'R'),
+                '#': (3, None, 'R'),
+                'B': (0, None, 'L')
+            },
+            4: {
+                '1': (4, 'B', 'R'),
+                'B': (4, None, 'L'),
+                '#': (5, 'B', 'L')
+            },
+            5: None
         },
-        1: {
-            '0': (1, None, 'L'),
-            '1': (1, None, 'L'),
-            '#': (2, None, 'L')
-        },
-        2: {
-            '0': (3, '1', 'N'),
-            '1': (2, '0', 'L'),
-            'B': (3, '1', 'N')
-        },
-        3: {
-            '0': (3, None, 'R'),
-            '1': (3, None, 'R'),
-            '#': (3, None, 'R'),
-            'B': (0, None, 'L')
-        },
-        4: {
-            '1': (4, 'B', 'R'),
-            'B': (4, None, 'L'),
-            '#': (5, 'B', 'L')
-        },
-        5: None
-    }
-)
+        # alphabet
+        {
+            "blank": "B",
+            "other": "01#"
+        }
+    )
 
-while turing.etape():
-    print(turing)
+    while turing.etape():
+        print(turing)
--- turing.py
+++ turing.py
@@ -1,14 +1,15 @@
 class Turing:
-    def __init__(self, position_lecture, bande, graphe):
+    def __init__(self, position_lecture, bande, graphe, alphabet):
         self.position_lecture = position_lecture
         
         # la bande est une liste de CHAINES DE CARACTERES
-        # (donc il faut bien mettre des chaînes pour les clés des dictionaires enfants,
-        # et quand on écrit dessus)
+        # (donc il faut bien mettre des chaînes pour les clés des dictionaires
+        # enfants, et quand on écrit dessus)
         self.bande = bande
         
         self.graphe = graphe
         self.etat = graphe.get("initial", 0)  # au cas où il n'est pas présent
+        self.alphabet = alphabet
     
     def __repr__(self):
         return f"{self.bande}\n{' ' * (self.position_lecture - 1)}^"
@@ -20,18 +21,21 @@ class Turing:
         transition = self.graphe[self.etat][self.bande[self.position_lecture]]
 
         if transition[1] != None: # écrire qqch si on le veut vraiment
-            self.bande[self.position_lecture] = transition[1]
+            if transition in self.alphabet["blank"] + self.alphabet["other"]:
+                self.bande[self.position_lecture] = transition[1]
+            else:
+                raise RuntimeError(f"Caractère n'appartenant pas à l'alphabet: {transition[1]} (possibles: {', '.join(self.alphabet['blank'] + self.alphabet['other'])})")
         
         if transition[2] == 'L':
             self.position_lecture -= 1
             if self.position_lecture < 0:
-                self.bande = ['B' for _ in range(16)] + self.bande
+                self.bande = [self.alphabet["blank"] for _ in range(16)] + self.bande
                 self.position_lecture += 16
         elif transition[2] == 'R':
             self.position_lecture += 1
             # augmenter la taille du ruban si besoin
             if self.position_lecture == len(self.bande):
-                self.bande += ['B' for _ in range(16)]
+                self.bande += [self.alphabet["blank"] for _ in range(16)]
         else:
             raise RuntimeError(f"Déplacement inconnu: {transition[2]} (possibles: L, R)")
 
